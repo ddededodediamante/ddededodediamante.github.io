1b36c0f5cfcea24d8bb87c08588fb11f468c1079
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2023-09-28 19:07:14 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const toolbox = {
+  'kind': 'flyoutToolbox',
+  'contents': [
+    {
+      'kind': 'block',
+      'type': 'controls_repeat_ext',
+      'inputs': {
+        'TIMES': {
+          'shadow': {
+            'type': 'math_number',
+            'fields': {
+              'NUM': 5
+            }
+          }
+        }
+      }
+    }
+  ]
+};
